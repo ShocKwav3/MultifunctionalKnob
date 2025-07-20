8e446fd29d08ffb745507da68f4f0f1bc7769a9c
ShocKwav3 <ahmonemon@gmail.com>
2025-07-21 01:37:48 +0300

    added mode enums

--- include/Enum/ModeEnum.h
+++ include/Enum/ModeEnum.h
@@ -2,6 +2,9 @@
 
 namespace ModeEnum {
     enum class AppEventTypes {
+        SCROLL,
+        VOLUME,
+        __MODE_SELECTION_LIMIT, // acts as the upper bound so we don’t loop into meta events accidentally
         MODE_SELECTION,
         MODE_SELECTED,
         MODE_SELECTION_CANCELLED
