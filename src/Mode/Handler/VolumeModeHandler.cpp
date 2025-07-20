563af5763a8d1874d019a5031888cc964d81e744
ShocKwav3 <ahmonemon@gmail.com>
2025-07-21 01:49:44 +0300

    added placeholder volume mode handler

---
+++ src/Mode/Handler/VolumeModeHandler.cpp
@@ -0,0 +1,20 @@
+#include "VolumeModeHandler.h"
+
+using namespace ModeEnum;
+
+VolumeModeHandler::VolumeModeHandler(AppEventDispatcher* dispatcher)
+    : AbstractModeHandler(dispatcher) {}
+
+void VolumeModeHandler::handleRotate(int delta) {
+    Serial.printf("VolumeModeHandler: Rotating by %d\n", delta);
+    // TODO: Send BLE horizontal scroll event via bleKeyboard.mouseMove(...)
+}
+
+void VolumeModeHandler::handleShortClick() {
+    Serial.println("VolumeModeHandler: Short click detected.");
+    // TODO: Maybe toggle scroll direction or switch sub-mode
+}
+
+const char* VolumeModeHandler::getModeName() const {
+    return ModeHelper::toString(AppEventTypes::VOLUME);
+}
