d0abd1e6d4a67b1418edcbfa342782833e48746b
ShocKwav3 <ahmonemon@gmail.com>
2025-07-21 01:49:28 +0300

    updated according to interface changes and dependencies

--- src/Mode/Handler/ScrollModeHandler.cpp
+++ src/Mode/Handler/ScrollModeHandler.cpp
@@ -1,5 +1,10 @@
 #include "ScrollModeHandler.h"
 
+using namespace ModeEnum;
+
+ScrollModeHandler::ScrollModeHandler(AppEventDispatcher* dispatcher)
+    : AbstractModeHandler(dispatcher) {}
+
 void ScrollModeHandler::handleRotate(int delta) {
     Serial.printf("ScrollModeHandler: Rotating by %d\n", delta);
     // TODO: Send BLE horizontal scroll event via bleKeyboard.mouseMove(...)
@@ -8,4 +13,8 @@ void ScrollModeHandler::handleRotate(int delta) {
 void ScrollModeHandler::handleShortClick() {
     Serial.println("ScrollModeHandler: Short click detected.");
     // TODO: Maybe toggle scroll direction or switch sub-mode
+}
+
+const char* ScrollModeHandler::getModeName() const {
+    return ModeHelper::toString(AppEventTypes::SCROLL);
 }
--- src/Mode/Handler/ScrollModeHandler.h
+++ src/Mode/Handler/ScrollModeHandler.h
@@ -3,12 +3,14 @@
 #include "Arduino.h"
 
 #include "AbstractModeHandler.h"
+#include "Enum/ModeEnum.h"
+#include "Helper/ModeHelper.h"
 
 class ScrollModeHandler : public AbstractModeHandler {
 public:
-    ScrollModeHandler(AppEventDispatcher* dispatcher)
-        : AbstractModeHandler(dispatcher) {}
+    ScrollModeHandler(AppEventDispatcher* dispatcher);
 
     void handleRotate(int delta) override;
     void handleShortClick() override;
+    const char* getModeName() const override;
 };
