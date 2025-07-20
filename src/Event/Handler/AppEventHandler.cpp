b44a2948c75cc2fd5204d84b379926338afd2829
ShocKwav3 <ahmonemon@gmail.com>
2025-07-21 01:43:08 +0300

    app event handler added; handles different modes

---
+++ src/Event/Handler/AppEventHandler.cpp
@@ -0,0 +1,41 @@
+#include "AppEventHandler.h"
+
+using namespace ModeEnum;
+
+AppEventHandler::AppEventHandler(QueueHandle_t queue, ModeManager* modeManager)
+    : eventQueue(queue), modeManager(modeManager) {}
+
+void AppEventHandler::start() {
+    xTaskCreate(taskEntry, "AppEventHandlerTask", 4096, this, 1, nullptr);
+}
+
+void AppEventHandler::taskEntry(void* param) {
+    static_cast<AppEventHandler*>(param)->taskLoop();
+}
+
+void AppEventHandler::taskLoop() {
+    AppEvent evt;
+
+    while (true) {
+        if (xQueueReceive(eventQueue, &evt, portMAX_DELAY)) {
+            Serial.print("AppEvent received: ");
+            Serial.println(ModeHelper::toString(evt.type));
+
+            if (evt.type == AppEventTypes::MODE_SELECTION) {
+                Serial.print("AppEvent mode selection");
+                modeManager->enterModeSelection();
+                //Serial.println(static_cast<int>(evt.type));
+            }
+            else if (evt.type == AppEventTypes::MODE_SELECTION_CANCELLED) {
+                Serial.print("AppEvent mode selection cancelled");
+                modeManager->cancelModeSelection();
+                //Serial.println(static_cast<int>(evt.type));
+            }
+            else if (static_cast<int>(evt.type) < static_cast<int>(AppEventTypes::__MODE_SELECTION_LIMIT)) {
+                Serial.print("AppEvent mode selection limit: ");
+                modeManager->setMode(evt.type);  // e.g., SCROLL, VOLUME, etc.
+                //Serial.println(static_cast<int>(evt.type));
+            }
+        }
+    }
+}
